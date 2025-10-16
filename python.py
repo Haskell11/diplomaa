74a5c8f843d162d3c106298c5ea9d9c85accf0d0
Maksim Kravtov <mkravtsov@i-gs.ru>
2025-10-16 10:40:15 +0300

    добавление логирования

--- python.py
+++ python.py
@@ -1,26 +1,51 @@
 import socket
 import time
+import os
+from datetime import datetime
 
 ESP32_IP = "192.168.1.26"  # IP ESP32 
 PORT = 3333
 
+# логи
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)  # создается папку
+
+# создарние уникальное имя файла по времени 
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_path = os.path.join(log_dir, f"log_{timestamp}.csv")
+
+# открытие файл для записи
+log_file = open(log_path, "w", encoding="utf-8")
+log_file.write("time,data\n")
+
+print(f"🧾 Логирование в файл: {log_path}")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(( "", PORT))
+sock.bind(("", PORT))
 
-print(f"📡 Connecting to ESP32 ({ESP32_IP}) ...")
+print(f" Connecting to ESP32 ({ESP32_IP}) ...")
 
-# команда на запуск
+# Отправляем команду на запуск
 sock.sendto(b"START", (ESP32_IP, PORT))
-print("🚀 Sent START command to ESP32")
+print("🚀 Sent START command to ESP32\n")
 
-
-while True:
-    try:
+try:
+    while True:
         data, addr = sock.recvfrom(1024)
-        print(f"📨 {addr}: {data.decode()}")
-    except KeyboardInterrupt:
-        print("🛑 Stopping...")
-        sock.sendto(b"STOP", (ESP32_IP, PORT))
-        break
+        msg = data.decode().strip()
+        current_time = time.strftime("%H:%M:%S")
 
-sock.close()
+        print(f"📨 {current_time} | {msg}")
+
+        # сохраняем данные в лог
+        log_file.write(f"{current_time},{msg}\n")
+        log_file.flush()  # сбрасываем буфер (на случай внезапного завершения)
+
+except KeyboardInterrupt:
+    print("\n🛑 Stopping...")
+    sock.sendto(b"STOP", (ESP32_IP, PORT))
+
+finally:
+    log_file.close()
+    sock.close()
+    print("📁 Лог сохранён и соединение закрыто.")
